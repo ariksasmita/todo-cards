c5af169906c2f58f62f11e081ad63f34aee55bf4
I Putu Arik Sasmita <ariksasmita@gmail.com>
2018-08-28 11:13:48 +0800

    Includes AddTodo component in TodoList

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -2,6 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 import Todo from './Todo'
+import AddTodo from './AddTodo'
 
 class TodoList extends React.Component {
   static propTypes = {
@@ -11,18 +12,35 @@ class TodoList extends React.Component {
   static defaultProps = {
     todos: [],
   }
+  state = {
+    showAddTodo: false,
+  }
+  onAddClick = () => {
+    const { showAddTodo } = this.state
+    this.setState({
+      showAddTodo: !showAddTodo,
+    })
+  }
   render() {
     const { 
       todos,
       onItemChange,
     } = this.props
+    const { showAddTodo } = this.state
+    const { onAddClick } = this
     const renderTodos = () => {
       return todos.map(todo => {
         return <Todo onItemChange={onItemChange} key={todo.id} {...todo} />
       })
     }
 
-    return <div>{renderTodos()}</div>
+    return (
+      <div>
+        <button onClick={ onAddClick }>[+]</button>
+        { showAddTodo && <AddTodo />}
+        {renderTodos()}
+      </div>
+    ) 
   }
 }
 
